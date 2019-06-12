1e90009460fd7c5d795e9c335411af1f18bdca19
lijiahe <lijiahe89757@qq.com>
2019-06-12 16:02:26 +0800

    添加swoole的第一个echo服务器

---
+++ swoole/Server.php
@@ -0,0 +1,41 @@
+<?php
+// Server
+class Server
+{
+    private $serv;
+
+    public function __construct() {
+        $this->serv = new swoole_server("127.0.0.1", 9501);
+        $this->serv->set(array(
+            'worker_num' => 8,
+            'daemonize' => false,
+        ));
+
+        $this->serv->on('Start', array($this, 'onStart'));
+        $this->serv->on('Connect', array($this, 'onConnect'));
+        $this->serv->on('Receive', array($this, 'onReceive'));
+        $this->serv->on('Close', array($this, 'onClose'));
+
+        $this->serv->start();
+    }
+
+    public function onStart( $serv ) {
+        echo "Start\n";
+    }
+
+    public function onConnect( $serv, $fd, $from_id ) {
+        echo "Connect\n";
+        $this->serv->send( $fd, "Hello {$fd}!\n" );
+    }
+
+    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
+        echo "Get Message From Client {$fd}:{$data}\n";
+        $this->serv->send($fd, $data);
+    }
+
+    public function onClose( $serv, $fd, $from_id ) {
+        echo "Client {$fd} close connection\n";
+    }
+}
+// 启动服务器 Start the server
+$server = new Server();
